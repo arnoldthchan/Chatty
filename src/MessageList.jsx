be95f844e6266e68002888658de0e65748642d68
Arnold Chan <arnoldthchan@gmail.com>
2017-07-25 02:05:38 +0000

    first commit, basic chattr skeleton

---
+++ src/MessageList.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+export default MessageList;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 // Application entrypoint.
 
 // Load up the application styles
-require("../styles/application.scss");
+require('../styles/application.scss');
 
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+// import App from './App.jsx';
+// import ChatBar from './ChatBar.jsx';
+// import Message from './Message.jsx';
+// import MessageList from './MessageList.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// ReactDOM.render(<App />, document.body);
+// ReactDOM.render(<ChatBar />, document.body);
+// ReactDOM.render(<Message />, document.getElementById('messages'));
+// ReactDOM.render(<MessageList />, document.getElementById('messages'));
